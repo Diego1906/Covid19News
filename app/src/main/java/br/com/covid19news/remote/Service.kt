bdd7c6048ebb6918c4da512ce70996dc444364d7
Diego <di_fsantos@yahoo.com.br>
2020-04-18 09:44:31 -0300

    Refatoração nas configurações do Retrofit
    Foi movida para o arquivo gradle.properties a URL_BASE, API_KEY e os header da requisição.

--- app/src/main/java/br/com/covid19news/remote/Service.kt
+++ app/src/main/java/br/com/covid19news/remote/Service.kt
@@ -1,5 +1,6 @@
 package br.com.covid19news.remote
 
+import br.com.covid19news.BuildConfig
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.OkHttpClient
@@ -9,20 +10,18 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 
 class Service : IService {
 
-    private val API_KEY = "81b8f30ce8msh2655e4470b99c3cp1219a3jsnceec1764dddd"
-
     private val retrofit: Retrofit
 
     init {
         val client = OkHttpClient.Builder()
             .addInterceptor { chain ->
                 val request: Request = chain.request().newBuilder()
-                    .addHeader("x-rapidapi-host", "covid-193.p.rapidapi.com")
-                    .addHeader("x-rapidapi-key", API_KEY)
+                    .addHeader(BuildConfig.HEADER_HOST_NAME, BuildConfig.HEADER_HOST_VALUE)
+                    .addHeader(BuildConfig.HEADER_API_KEY_NAME, BuildConfig.API_KEY)
                     .build()
 
                 chain.proceed(request)
-            }
+            }.build()
 
         val moshi = Moshi.Builder()
             .add(KotlinJsonAdapterFactory())
@@ -30,8 +29,8 @@ class Service : IService {
 
         retrofit = Retrofit.Builder()
             .addConverterFactory(MoshiConverterFactory.create(moshi))
-            .baseUrl("https://covid-193.p.rapidapi.com/")
-            .client(client.build())
+            .baseUrl(BuildConfig.URL_BASE)
+            .client(client)
             .build()
     }
 
